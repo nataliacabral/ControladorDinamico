8121789e114ef6d9fcddd20bc6429c6370b5bc96
Natália Cabral <naatcabral@gmail.com>
2015-01-06 17:01:54 -0300

    adicionndo imgs

--- ControladorDinamico/ControladorDinamico/ViewController.swift
+++ ControladorDinamico/ControladorDinamico/ViewController.swift
@@ -25,13 +25,6 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 
         var projectCell:UINib = UINib(nibName:"ProjectView", bundle: nil)
         self.collectionView.registerNib(projectCell, forCellWithReuseIdentifier:projectCellViewIdentifier)
-        
-        var longPresGestureRecognizer:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
-        longPresGestureRecognizer.minimumPressDuration = 0.5
-        longPresGestureRecognizer.delegate = self
-        longPresGestureRecognizer.delaysTouchesBegan = true
-        self.collectionView.addGestureRecognizer(longPresGestureRecognizer)
-
     }
     
     func addTextField(textField: UITextField!){
@@ -70,53 +63,9 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         if(ProjectManager.sharedInstance.removeProject(self.selectedProject!, error: &error)) {
             self.collectionView.reloadData()
         }
+        self.selectedProject = nil
     }
     
-    func handleLongPress(gestureRecognizer:UILongPressGestureRecognizer)
-    {
-        var cell:UICollectionViewCell?
-        var touchedProject:Project?
-
-        let point:CGPoint = gestureRecognizer.locationInView(self.collectionView)
-        let indexPath:NSIndexPath? = self.collectionView.indexPathForItemAtPoint(point)
-        
-        if (indexPath != nil) {
-            cell = self.collectionView.cellForItemAtIndexPath(indexPath!)
-        
-            if (indexPath!.row < self.projects.count) {
-                touchedProject = self.projects[indexPath!.row] as? Project
-            }
-        }
-        
-        switch gestureRecognizer.state {
-        case .Began:
-            if (cell != nil) {
-                cell!.backgroundColor = UIColor.grayColor()
-                if (touchedProject != nil) {
-                    self.selectedProject = touchedProject
-                }
-            }
-            
-            break
-        case .Changed, .Ended:
-            if (cell != nil) {
-                cell!.backgroundColor = UIColor.blackColor()
-                if (self.selectedProject != nil) {
-                    
-                    var deleteAlertController = UIAlertController(title: "Delete?", message:"Are you sure you want to delete this project?", preferredStyle: UIAlertControllerStyle.Alert)
-                    deleteAlertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
-                    deleteAlertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: deleteProject))
-
-                    self.presentViewController(deleteAlertController, animated: true, completion: nil)
-
-                }
-            }
-            break
-        
-        default:
-            break
-        }
-    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewDidAppear(animated)
@@ -125,43 +74,62 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return projects.count + 1;
+        return projects.count;
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         var collectionViewCell:UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(projectCellViewIdentifier, forIndexPath: indexPath) as UICollectionViewCell
-        collectionViewCell.backgroundColor = UIColor.blackColor()
-       
-        if (indexPath.row < projects.count) {
-            let project:Project = (projects.objectAtIndex(indexPath.row) as Project)
-            let projectNameLabel:UILabel = collectionViewCell.viewWithTag(projectNameLabelTag) as UILabel
-            projectNameLabel.text = project.projectName
-            
-            let previewView:UIImageView = collectionViewCell.viewWithTag(projectBackgroundViewTag) as UIImageView
-            previewView.image = project.preview
-            return collectionViewCell
         
+        if (collectionViewCell.selected) {
+            collectionViewCell.backgroundColor = UIColor.orangeColor()
         } else {
-            
-            let projectNameLabel:UILabel = collectionViewCell.viewWithTag(projectNameLabelTag) as UILabel
-            projectNameLabel.text = "+"
-            let previewView:UIImageView = collectionViewCell.viewWithTag(projectBackgroundViewTag) as UIImageView
-            previewView.image = nil
-            return collectionViewCell
+            collectionViewCell.backgroundColor = UIColor.grayColor()
         }
+        
+        let project:Project = (projects.objectAtIndex(indexPath.row) as Project)
+        let projectNameLabel:UILabel = collectionViewCell.viewWithTag(projectNameLabelTag) as UILabel
+        projectNameLabel.text = project.projectName
+        
+        let previewView:UIImageView = collectionViewCell.viewWithTag(projectBackgroundViewTag) as UIImageView
+        previewView.image = project.preview
+    
+        return collectionViewCell
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let datasetCell = collectionView.cellForItemAtIndexPath(indexPath)
+        datasetCell?.backgroundColor = UIColor.orangeColor()
+        
         if (projects.count > indexPath.row) {
             self.selectedProject = self.projects.objectAtIndex(indexPath.row) as? Project
-            self.performSegueWithIdentifier("openProject", sender: self)
         } else {
-            let newProjectAlertController = UIAlertController(title: "New Project", message: "Insert the project name:", preferredStyle: UIAlertControllerStyle.Alert)
-            newProjectAlertController.addTextFieldWithConfigurationHandler(addTextField)
-            newProjectAlertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
-            newProjectAlertController.addAction(UIAlertAction(title: "Save", style: UIAlertActionStyle.Default, handler: newProjectEntered))
-            presentViewController(newProjectAlertController, animated: true, completion: nil)
+            self.selectedProject = nil
         }
+
+    }
+    
+    func collectionView(collectionView: UICollectionView, didDeselectItemAtIndexPath indexPath: NSIndexPath) {
+        let datasetCell = collectionView.cellForItemAtIndexPath(indexPath)
+        datasetCell?.backgroundColor = UIColor.grayColor()
+    }
+    
+    @IBAction func removeProject(AnyObject) {
+        if (self.selectedProject != nil) {
+            
+            var deleteAlertController = UIAlertController(title: "Delete?", message:"Are you sure you want to delete this project?", preferredStyle: UIAlertControllerStyle.Alert)
+            deleteAlertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+            deleteAlertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: deleteProject))
+            self.presentViewController(deleteAlertController, animated: true, completion: nil)
+            
+        }
+    }
+    
+    @IBAction func addProject(AnyObject) {
+        let newProjectAlertController = UIAlertController(title: "New Project", message: "Insert the project name:", preferredStyle: UIAlertControllerStyle.Alert)
+        newProjectAlertController.addTextFieldWithConfigurationHandler(addTextField)
+        newProjectAlertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+        newProjectAlertController.addAction(UIAlertAction(title: "Save", style: UIAlertActionStyle.Default, handler: newProjectEntered))
+        presentViewController(newProjectAlertController, animated: true, completion: nil)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
