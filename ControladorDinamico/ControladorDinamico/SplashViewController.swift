5a6b7f984a2d51f1ef329df57e8aa08ff3c7c3a1
Natália Cabral <natalia_cabral@apple.com>
2015-04-15 21:09:28 -0300

    Tela de creditos

---
+++ ControladorDinamico/ControladorDinamico/SplashViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SplashViewController.swift
+//  ControladorDinamico
+//
+//  Created by Natália Cabral on 4/15/15.
+//  Copyright (c) 2015 Natália Cabral. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController : UIViewController {
+    @IBOutlet weak var content: UIImageView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("updateSplash"), userInfo: nil, repeats: false)
+    }
+    
+    
+    func updateSplash() {
+        self.content?.image = UIImage(named: "edit_empty.jpg")
+        var timer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("closeSplash"), userInfo: nil, repeats: false)
+    }
+    
+    func closeSplash() {
+        self.performSegueWithIdentifier("homeSegue", sender: self)
+    }
+}
